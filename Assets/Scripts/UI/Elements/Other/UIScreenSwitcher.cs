4e36db81dc4ea895a5a017ba734ff7a537850dc2
azule <maksim.evdokimov.work@gmail.com>
2026-04-05 02:44:16 +0300

    ui: settings (wip)

---
+++ Assets/Scripts/UI/Elements/Other/UIScreenSwitcher.cs
@@ -0,0 +1,45 @@
+using System;
+using UnityEngine;
+
+
+namespace UI.Elements.Other
+{
+	public class UIScreenSwitcher : MonoBehaviour
+	{
+		[Serializable]
+		public class Screen
+		{
+			[field: SerializeField] public string     Name   { get; private set; }
+			[field: SerializeField] public GameObject Object { get; private set; }
+		}
+
+
+		[SerializeField] private Screen[] m_Screens;
+		private                  int      m_ActiveScreenIndex = -1;
+
+		public void Switch(int index)
+		{
+			if (m_ActiveScreenIndex == index) return;
+			if (index < 0 || index >= m_Screens.Length) {
+				Debug.LogError($"[{nameof(UIScreenSwitcher)}] Invalid screen index: {index}");
+				return;
+			}
+
+			if (m_ActiveScreenIndex != -1)
+				m_Screens[m_ActiveScreenIndex].Object.SetActive(false);
+
+			m_ActiveScreenIndex = index;
+			m_Screens[m_ActiveScreenIndex].Object.SetActive(true);
+		}
+		public void Switch(string screenName)
+		{
+			int index = Array.FindIndex(m_Screens, s => s.Name == screenName);
+			if (index == -1) {
+				Debug.LogError($"[{nameof(UIScreenSwitcher)}] No screen found with name: {screenName}");
+				return;
+			}
+
+			Switch(index);
+		}
+	}
+}
