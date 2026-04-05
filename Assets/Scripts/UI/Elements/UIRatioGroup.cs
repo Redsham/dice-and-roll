dc889bed7267ae9affaa5f193d6895ad63f47c78
azule <maksim.evdokimov.work@gmail.com>
2026-04-05 16:38:00 +0300

    fixes

--- Assets/Scripts/UI/Elements/UIRatioGroup.cs
+++ Assets/Scripts/UI/Elements/UIRatioGroup.cs
@@ -8,36 +8,25 @@ namespace UI.Elements
 {
 	public class UIRatioGroup : MonoBehaviour
 	{
+		// State
+
 		public int               SelectedIndex { get; private set; } = -1;
 		public event Action<int> OnSelected = delegate { };
 
 		[SerializeField] private int m_DefaultIndex = 0;
 		[SerializeField] private bool m_Loop;
 		
-		private UIRatioButton[] m_Buttons = Array.Empty<UIRatioButton>();
+		private UIRatioButton[] m_Buttons     = Array.Empty<UIRatioButton>();
+		private bool            m_Initialized = false;
 
-		private void Start()
+		// Lifecycle
+
+		private void Awake()
 		{
-			// Find all UIRatioButton components in children
-			m_Buttons = GetComponentsInChildren<UIRatioButton>();
-			if (m_Buttons.Length == 0) {
-				Debug.LogError($"[{nameof(UIRatioGroup)}] No UIRatio buttons found");
-				enabled = false;
-				return;
-			}
-
-			// Set up button listeners
-			for (int i = 0; i < m_Buttons.Length; i++) {
-				int index = i;
-
-				m_Buttons[i].Index = i;
-				m_Buttons[i].OnClick.AddListener(() => Select(m_Buttons[index].Index));
-				m_Buttons[i].OnDeselect();
-			}
-
-			// Select first element
-			Select(m_DefaultIndex);
+			EnsureInitialized();
 		}
+
+		// Selection
 
 		/// <summary>
 		/// Select the button at the specified index
@@ -45,8 +34,12 @@ namespace UI.Elements
 		/// <param name="index">Index of the button to select</param>
 		public void Select(int index, bool notify = true)
 		{
+			if (!EnsureInitialized()) {
+				return;
+			}
+
 			if (SelectedIndex == index) return;
-			if (index < 0 && index >= m_Buttons.Length) {
+			if (index < 0 || index >= m_Buttons.Length) {
 				Debug.LogError($"[{nameof(UIRatioGroup)}] Invalid index: {index}");
 				return;
 			}
@@ -67,6 +60,10 @@ namespace UI.Elements
 		/// </summary>
 		public void Next()
 		{
+			if (!EnsureInitialized()) {
+				return;
+			}
+
 			if (SelectedIndex == m_Buttons.Length - 1) {
 				if (m_Loop) Select(0);
 				else return;
@@ -80,12 +77,44 @@ namespace UI.Elements
 		/// </summary>
 		public void Previous()
 		{
+			if (!EnsureInitialized()) {
+				return;
+			}
+
 			if (SelectedIndex == 0) {
 				if (m_Loop) Select(m_Buttons.Length - 1);
 				else return;
 			}
 			
 			Select(SelectedIndex - 1);
+		}
+
+		// Helpers
+
+		private bool EnsureInitialized()
+		{
+			if (m_Initialized) {
+				return m_Buttons.Length > 0;
+			}
+
+			m_Buttons = GetComponentsInChildren<UIRatioButton>(true);
+			if (m_Buttons.Length == 0) {
+				Debug.LogError($"[{nameof(UIRatioGroup)}] No UIRatio buttons found");
+				enabled = false;
+				return false;
+			}
+
+			for (int i = 0; i < m_Buttons.Length; i++) {
+				int index = i;
+
+				m_Buttons[i].Index = i;
+				m_Buttons[i].OnClick.AddListener(() => Select(m_Buttons[index].Index));
+				m_Buttons[i].OnDeselect();
+			}
+
+			m_Initialized = true;
+			Select(Mathf.Clamp(m_DefaultIndex, 0, m_Buttons.Length - 1), false);
+			return true;
 		}
 	}
 }
